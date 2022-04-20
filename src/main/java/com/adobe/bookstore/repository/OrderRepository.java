af8e6afef97e6a0c4deb2682b51e041554ef07c2
David Ruiz <ruiz.6789@gmail.com>
2022-04-20 17:28:49 +0200

    Change Order repository key from the interface it's extending

--- src/main/java/com/adobe/bookstore/repository/OrderRepository.java
+++ src/main/java/com/adobe/bookstore/repository/OrderRepository.java
@@ -4,6 +4,8 @@ import com.adobe.bookstore.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 }
